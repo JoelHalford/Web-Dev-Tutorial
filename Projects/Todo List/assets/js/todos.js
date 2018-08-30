3d8c2d930120d90265de6763cdac854139017e63
topsup <topsupp@hotmail.com>
2018-08-31 00:06:42 +0100

    Adding todos
    Used append to add new todos to list when enter key is pressed.
    Appended functions to work with future todos.

--- Projects/Todo List/assets/js/todos.js
+++ Projects/Todo List/assets/js/todos.js
@@ -1,13 +1,25 @@
 // Check off Specific Todos By Clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$(".delete").click(function(event){
+$("ul").on("click", ".delete", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event){
+
+	if(event.which === 13)
+	{	//grab new todo text from input
+		var todoText = $(this).val();
+		//clear input
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span class='delete'>X</span> " + todoText + "</li>");
+	};
 });
